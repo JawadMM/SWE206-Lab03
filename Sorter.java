6f389c05cf170dee9c0d10db85ac6df9526648b5
JawadMM <Almuttawa.j@hotmail.com>
2023-02-14 18:18:13 +0300

    commit message

--- Sorter.java
+++ Sorter.java
@@ -24,6 +24,21 @@ public class Sorter {
         }
     }
 
+    public static void insertionSort(double[] array) {
+        
+        for (int i = 0; i < array.length; i++) {
+
+            double currentElement = array[i];
+            int k;
+
+            for (k = i - 1; k >= 0 && currentElement < array[k]; k--) {
+                array[k+1] = array[k];
+            }
+
+            array[k + 1] = currentElement;
+        }
+    }
+
     public static void Sort(double[] array) {
         selectionSort(array);
     }

cf8ffc083ab093d96bf1c593fc7671740bc5b1a1
JawadMM <Almuttawa.j@hotmail.com>
2023-02-14 14:00:25 +0300

    commit message

--- Sorter.java
+++ Sorter.java
@@ -1,5 +1,26 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Sorter {
     public static void main(String[] args) {
         
+    }
+
+    public static ArrayList<String> readStu(){
+        ArrayList <String> arr = new ArrayList<>();
+
+        try {
+            
+            File f = new File("Student List.txt");
+            Scanner sc = new Scanner(f);
+            while(sc.hasNextLine()){
+
+                arr.add(sc.nextLine());
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        return arr;
     }
 }

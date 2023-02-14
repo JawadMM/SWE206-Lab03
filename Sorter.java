b6b1da54baf552aa50f6bc8c1b0034dd2d21c36c
JawadMM <Almuttawa.j@hotmail.com>
2023-02-14 16:22:06 +0300

    commit message

--- Sorter.java
+++ Sorter.java
@@ -5,4 +5,23 @@ public class Sorter {
         
     }
 
+    public static void selectionSort(Double[] array) {
+        for (int i = 0; i < array.length; i++) {
+            double currentMin = array[i];
+            int currentMinIndex = i;
+
+            for (int j = i + 1; j < array.length; j++) {
+                if (currentMin > array[j]) {
+                    currentMin = array[j];
+                    currentMinIndex = j;
+                }
+            }
+
+            if (currentMinIndex != i) {
+                array[currentMinIndex] = array[i];
+                array[i] = currentMin;
+            }
+        }
+    }
+
 }
